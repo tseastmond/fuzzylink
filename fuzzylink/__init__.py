dd74853880a4938e134ebc3d1e74283a3f7a3335
Tanner S Eastmond <32971227+tseastmond@users.noreply.github.com>
2020-07-10 16:13:15 -0700

    Add files via upload
    Reorganize the package

---
+++ fuzzylink/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Manager
+import sys
+from time import time
+
+from jellyfish import jaro_winkler
+import numpy as np
+import pandas as pd
+
+from .distance_matching import GetNClosest
+from .matching import Match
+
+__title__ = 'fuzzylink'
+__version__ = '1.0.0'
+__author__ = 'Tanner S Eastmond'
+__contact__ = 'https://github.com/tseastmond'
+__license__ = 'MIT'
+
